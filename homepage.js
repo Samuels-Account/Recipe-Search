331638a8baf497f437d8c9dc75865d4704fa9b4b
Samuel Ledun <sledun@outlook.com>
2023-05-05 18:47:30 +0100

    Removed Js code in attempt to connect a recipe API

--- homepage.js
+++ homepage.js
@@ -15,19 +15,4 @@ indicators.forEach((item, i) => {
 
 
 
-function openNav() {
-    document.getElementById("sidebar").style.width = "250px";
-    document.getElementById("hero").style.marginLeft = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("sidebar").style.width = "0";
-    document.getElementById("hero").style.marginLeft = "0";
-  }
-  
-  const apiKey = 'eda054d33c5533624be7306db4aadd60';
-  const appId = 'f84041fc';  
-  const query = 'chicken'; // Replace with the search term for the recipe you want to feature
-  const apiUrl = `https://api.edamam.com/search?q=chicken&app_id=f84041fc&app_key=eda054d33c5533624be7306db4aadd60`;
-
   
