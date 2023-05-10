3b96b18c43651d067c00af4ecd693df6ae5f3d7a
Samuel Ledun <sledun@outlook.com>
2023-05-10 09:37:38 +0100

    Link pages html pages together

--- homepage.js
+++ homepage.js
@@ -4,24 +4,24 @@ const indicators = [...document.querySelectorAll('.indicators button')];
 let currentTestimonial = 0; // Default 0
 
 indicators.forEach((item, i) => {
-    item.addEventListener('click', () => {
-        indicators[currentTestimonial].classList.remove('active');
-        wrapper.style.marginLeft = `-${100 * i}%`;
-        item.classList.add('active');
-        currentTestimonial = i;
-    })
+  item.addEventListener('click', () => {
+    indicators[currentTestimonial].classList.remove('active');
+    wrapper.style.marginLeft = `-${100 * i}%`;
+    item.classList.add('active');
+    currentTestimonial = i;
+  })
 })
 
 
 function openNav() {
-    document.getElementById("sidebar").style.width = "250px";
-    document.getElementById("hero").style.marginLeft = "250px";
-  }
+  document.getElementById("sidebar").style.width = "250px";
+  document.getElementById("hero").style.marginLeft = "250px";
+}
 
-  function closeNav() {
-    document.getElementById("sidebar").style.width = "0";
-    document.getElementById("hero").style.marginLeft = "0";
-  }
+function closeNav() {
+  document.getElementById("sidebar").style.width = "0";
+  document.getElementById("hero").style.marginLeft = "0";
+}
 
 
 
